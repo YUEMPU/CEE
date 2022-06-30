0284c9ec3888ca570a5f4427e68899061f8bbaed
YUEMPU <107873576+YUEMPU@users.noreply.github.com>
2022-06-30 22:19:43 +0800

    6-30初识操作符

--- VS2019/test-P19/test-P19/test.c
+++ VS2019/test-P19/test-P19/test.c
@@ -1,11 +1,59 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include <stdio.h>
-int main()
-{
-	int a = 0;
-	printf("%d\n", ~a);
-
-	return 0;
-
-}
+//int main()
+//{
+//	int a = 0;
+//	printf("%d\n", ~a);
+//
+//	return 0;
+//
+//}
+//
+//int main()
+//{
+	//int a = 10;
+	//int b = a++;//后置++ 先使用 再++
+	//int a = 10;
+	//int b = ++a;//前置++ 先++ 再使用
+	//printf("%d\n", b);
+	//printf("%d\n", a);
+//
+//	return 0;
+//
+//
+//
+//int main()
+//{
+//	int a = (int)3.14;//强制类型转换
+//	printf("%d\n", a);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = 0;
+//	int c = a || b;//或者	逻辑或
+//	int c = a && b;//并且	逻辑与
+//
+//	return 0;
+////}
+//int main()
+//{
+//	int a = 0;
+//	int b = 3;
+//	int c = 5;
+//			//a=5		c=1			b=3
+//	int d = (a = b + 2, c = a - 4, b = c + 2);
+//	//逗号表达式，从左到右依次计算
+//	//整个表达式的结果是最后一个表达式的结果
+//	printf("%d\n", d);
+//	return 0;
+////}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d\n", arr[0]);
+//					//下标引用操作符
+//	return 0;
+//}
